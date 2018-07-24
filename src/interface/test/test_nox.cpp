739ee6e03c97b9ce647ede7ae17942fd47001979
Tim Fuller <tjfulle@sandia.gov>
2018-07-24 16:18:44 -0600

    NOXSolver

--- src/interface/test/test_nox.cpp
+++ src/interface/test/test_nox.cpp
@@ -14,17 +14,8 @@
 #include <Tpetra_Core.hpp>
 #include <Tpetra_Vector.hpp>
 
-#include <BelosTypes.hpp>
-#include <Stratimikos_DefaultLinearSolverBuilder.hpp>
-#include <Thyra_LinearOpWithSolveFactoryHelpers.hpp>
-#include <Thyra_Ifpack2PreconditionerFactory.hpp>
-
-#include <NOX.H>
-#include <NOX_Thyra.H>
-#include <NOX_Thyra_MatrixFreeJacobianOperator.hpp>
-#include <NOX_MatrixFree_ModelEvaluatorDecorator.hpp>
-
 #include "Tpetra_ModelEvaluator_1DFEM.hpp"
+#include "nox_solver.hpp"
 
 const Tpetra::global_size_t numGlobalElements = 100;
 
@@ -43,126 +34,20 @@ int main2(Teuchos::RCP<const Teuchos::Comm<int>>& comm,
   using GO     = long long;
   using Node   = Kokkos::Compat::KokkosSerialWrapperNode;
 
-  RCP<Teuchos::FancyOStream> out = Teuchos::VerboseObjectBase::getDefaultOStream();
-
   Teuchos::TimeMonitor::zeroOutTimers();
 
   // Create the model evaluator object
-  Scalar x00 = 0.0;
-  Scalar x01 = 1.0;
   RCP<TpetraModelEvaluator1DFEM<Scalar,LO,GO,Node>> model =
-    tpetraModelEvaluator1DFEM<Scalar,LO,GO,Node>(comm, numGlobalElements, x00, x01);
-
-#if 1
+    tpetraModelEvaluator1DFEM<Scalar,LO,GO,Node>(comm, numGlobalElements, 0.0, 1.0);
   model->setup_linear_solver(plist);
-#else
-  Stratimikos::DefaultLinearSolverBuilder builder;
 
-  {
-    auto p = Teuchos::rcpFromRef(plist->sublist("Linear Solver Settings"));
+  ForTrilinos::NOXSolver<Scalar,LO,GO,Node> nox_solver(model);
+  nox_solver.build(plist);
+  nox_solver.solve();
 
-    std::string prec = p->get("Preconditioner Type", "None");
-    if (prec == "None") {
-      // Do nothing
-    } else if (prec == "Ifpack2") {
-      using Base = Thyra::PreconditionerFactoryBase<Scalar>;
-      using Impl = Thyra::Ifpack2PreconditionerFactory<Tpetra::CrsMatrix<Scalar,LO,GO,Node>>;
-      builder.setPreconditioningStrategyFactory(Teuchos::abstractFactoryStd<Base, Impl>(), "Ifpack2");
-    } else {
-      TEUCHOS_TEST_FOR_EXCEPTION(true, std::invalid_argument,
-        "Preconditioner Type must be one of 'None', 'Ifpack2'")
-    }
-    builder.setParameterList(p);
-  }
+  NOX::StatusTest::StatusType solve_status = nox_solver.solve();
 
-  RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar>> lows_factory =
-    builder.createLinearSolveStrategy("");
-
-  model->set_W_factory(lows_factory);
-
-  // Create the initial guess
-  RCP<Thyra::VectorBase<Scalar>> initial_guess =
-    model->getNominalValues().get_x()->clone_v();
-  Thyra::V_S(initial_guess.ptr(),Teuchos::ScalarTraits<Scalar>::one());
-#endif
-
-  // Create the JFNK operator
-  std::string jtype;
-  RCP<NOX::Thyra::Group> nox_group;
-  RCP<Thyra::PreconditionerBase<Scalar>> prec_op;
-  RCP<Thyra::ModelEvaluator<Scalar>> thyra_model;
-  RCP<NOX::Thyra::MatrixFreeJacobianOperator<Scalar>> jfnk_op;
-
-  if (plist->isSublist("Jacobian Settings")) {
-    auto jsettings = plist->sublist("Jacobian Settings");
-    jtype = jsettings.get("Jacobian Type", "Analytic");
-    if (jtype == "Analytic") {
-
-      // Create the NOX::Thyra::Group
-      nox_group = rcp(new NOX::Thyra::Group(*(model->initial_guess), model,
-                                            model->create_W_op(), model->lows_factory,
-                                            Teuchos::null, Teuchos::null));
-
-    } else {
-      auto p = jsettings.sublist("Jacobian Types");
-      if (jtype == "Matrix Free Newton") {
-        auto jfnk_params = Teuchos::rcpFromRef(p.sublist(jtype));
-        Teuchos::ParameterList print_params;
-        jfnk_op = rcp(new NOX::Thyra::MatrixFreeJacobianOperator<Scalar>(print_params));
-        jfnk_op->setParameterList(jfnk_params);
-        jfnk_params->print(*out);
-
-        // Wrap the model evaluator in a JFNK Model Evaluator
-        thyra_model = rcp(new NOX::MatrixFreeModelEvaluatorDecorator<Scalar>(model));
-
-        // Create the Preconditioner operator
-        if (jsettings.get<bool>("Use Prec", false))
-          prec_op = thyra_model->create_W_prec();
-
-        // Create the NOX::Thyra::Group
-        nox_group = rcp(new NOX::Thyra::Group(*(model->initial_guess), thyra_model, jfnk_op,
-                                              model->lows_factory, prec_op, Teuchos::null));
-
-      }
-    }
-  } else {
-    // Default to Analytic Jacobian
-    nox_group = rcp(new NOX::Thyra::Group(*(model->initial_guess), model,
-                                          model->create_W_op(), model->lows_factory,
-                                          Teuchos::null, Teuchos::null));
-  }
-
-  if (nox_group.is_null())
-    TEUCHOS_TEST_FOR_EXCEPTION(true, std::invalid_argument, "Unkown Jacobian type " << jtype);
-
-  nox_group->computeF();
-
-  // VERY IMPORTANT!!!  jfnk object needs base evaluation objects.
-  // This creates a circular dependency, so use a weak pointer.
-  if (!jfnk_op.is_null())
-    jfnk_op->setBaseEvaluationToNOXGroup(nox_group.create_weak());
-
-  // Create the NOX status tests and the solver
-  // Create the convergence tests
-  RCP<NOX::StatusTest::Combo> converged = rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::AND));
-  RCP<NOX::StatusTest::NormF> absresid = rcp(new NOX::StatusTest::NormF(1.0e-8));
-  RCP<NOX::StatusTest::NormWRMS> wrms = rcp(new NOX::StatusTest::NormWRMS(1.0e-2, 1.0e-8));
-  converged->addStatusTest(absresid);
-  converged->addStatusTest(wrms);
-
-  RCP<NOX::StatusTest::Combo> combo = rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::OR));
-  RCP<NOX::StatusTest::MaxIters> maxiters = rcp(new NOX::StatusTest::MaxIters(20));
-  RCP<NOX::StatusTest::FiniteValue> fv = rcp(new NOX::StatusTest::FiniteValue);
-  combo->addStatusTest(fv);
-  combo->addStatusTest(converged);
-  combo->addStatusTest(maxiters);
-
-  // Create the solver
-  auto nl_params = Teuchos::sublist(plist, "Nonlinear Solver Settings");
-  RCP<NOX::Solver::Generic> solver = NOX::Solver::buildSolver(nox_group, combo, nl_params);
-  NOX::StatusTest::StatusType solvStatus = solver->solve();
-
-  int returncode = (solvStatus == NOX::StatusTest::Converged) ? 0 : 1;
+  int returncode = (solve_status == NOX::StatusTest::Converged) ? 0 : 1;
 
   Teuchos::TimeMonitor::summarize();
 
